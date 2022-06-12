c2eda896f21de5822fef875a2b2682ac921192b0
Your Name <you@example.com>
2022-06-12 16:06:28 +0700

    Training While

--- usewhile.js
+++ usewhile.js
@@ -1,7 +1,7 @@
-let num=+prompt('Enter a number');
-let total=0;
+let num= parseInt(prompt('Nhập 1 số'));
+let total=num;
 while (num != -1){
-    num=+prompt('Enter a number');
-    alert(num);
+    num = +prompt('Nhập 1 số');
+    total += num;
 }
-document.write(total);
+alert(total);
